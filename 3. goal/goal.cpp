66adb3fc3097070d2f29116466bb39be6ab84615
Ben <ben.c.hawker@gmail.com>
2016-04-18 22:58:36 +0800

    Formatting

--- 3. goal/goal.cpp
+++ 3. goal/goal.cpp
@@ -2,30 +2,33 @@
 
 int main()
 {
-	//initialize array with 4 elements (all floats)
-	float goal[4];
-	goal[0] = 0.8;
-	goal[1] = 0.6;
-	goal[2] = 0.4;
-	goal[3] = 0.2;
-	//Define 2 variables as floats
-	float current, target;
+  string output = ''
 
-	//Get use input. Save input into variable
-	std::cout << "Enter current weight: ";
-	std::cin >> current;
+  for (int i = 0; i < 100; i++)
+  {
+    if (i % 3)
+    {
+      output << "Fizz";
+      return 0;
+    }
 
-	std::cout << "Enter your targeted weight: ";
-	std::cin >> target;
+    if (i % 5)
+    {
+      output << "Buzz";
+      return 0;
+    }
 
-	//For loop.
-	for (int i = 0; i < 4; i++)
-	{
-		float loss = (current - target) * goal[i];
-		std::cout << "Goal " << i << ": ";
-		std::cout << current - loss << "\n";
-	}
+    if (i % 3) && (i % 5)
+    {
+      output << "FizzBuzz";
+      return 0;
+    }
+    else
+    {
+      output << i;
+    }
+  }
+  std::cout << output;
+  return 0;
 
-	//Function must return int. main function returns 0;
-	return 0;
 }
--- 4. fizzbuzz/fizzbuzz_2.cpp
+++ 4. fizzbuzz/fizzbuzz_2.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-	for(int i = 1; i <= 100; i++) 
-	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			puts ("FizzBuzz");
-		}
-		if (i % 3 == 0)
-		{
-			puts ("Fizz");
-		}
-		if (i % 5 == 0)
-		{
-			puts ("Buzz");
-		}
-		else
-		{
-			printf ("%i\n", i);
-		}
-	}
+  for(int i = 1; i <= 100; i++)
+  {
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+      puts ("FizzBuzz");
+    }
+    if (i % 3 == 0)
+    {
+      puts ("Fizz");
+    }
+    if (i % 5 == 0)
+    {
+      puts ("Buzz");
+    }
+    else
+    {
+      printf ("%i\n", i);
+    }
+  }
 }
 
 
